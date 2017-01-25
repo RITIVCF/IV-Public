4cc2e618b9410112a9d4c733b5966657708a83a7
awe6013 <awe6013@rit.edu>
2017-01-24 23:43:44 -0500

    Added Small Group Email functionality. Added Churches Interested funcitonality. Finished Contact Form functionality.

--- client/main.js
+++ client/main.js
@@ -2,3 +2,12 @@ import '../imports/startup/client/main.js';
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+
+// This is to correct the materialize toast error:
+//    'Vel is not defined'
+if ($) {
+  Vel = $.Velocity;
+}
+else {
+  Vel = Velocity; // change value with jQuery.Velocity
+}
